bd2f997a3eef5dfd972162c2147835749f87aa6a
Fatso0112 <farai.m.mwenje@gmail.com>
2026-02-13 15:10:37 +0200

    Fix backends

--- backend/src/routes/adminReportRoutes.js
+++ backend/src/routes/adminReportRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authMiddleware = require('../middleware/authmiddleware');
+const authMiddleware = require('../middleware/authMiddleware');
 const adminMiddleware = require('../middleware/adminMiddleware');
 const { readData } = require('../utils/fileHandler');
 

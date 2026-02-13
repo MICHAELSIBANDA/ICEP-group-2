17d101772b535eab9bdcd7fe8561df1e49ba78b7
Fatso0112 <farai.m.mwenje@gmail.com>
2026-02-13 15:33:09 +0200

    frontend deployment issues fixed

--- frontend/src/pages/adminAnnouncements.jsx
+++ frontend/src/pages/adminAnnouncements.jsx
@@ -14,7 +14,7 @@ function AdminAnnouncements() {
     setSuccess("");
 
     const res = await fetch(
-      "http://localhost:3001/api/announcements",
+      `${import.meta.env.VITE_API_URL}/api/auth/announcements`,
       {
         method: "POST",
         headers: {

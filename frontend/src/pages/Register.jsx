d7868d51ebc5bd854212d2017a43a81124fc92d3
Lesego Mfikoe <mfikoelucky@gmail.com>
2026-02-15 19:57:47 +0200

    Login and Register links fix attempt #4

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
+
 
 function Register() {
   const [studentNumber, setStudentNumber] = useState("");

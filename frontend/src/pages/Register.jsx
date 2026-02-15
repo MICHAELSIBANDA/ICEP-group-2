fa260c48a60acf33ab2d42118a4fc0547a3c5f4f
Fatso0112 <farai.m.mwenje@gmail.com>
2026-02-15 19:04:19 +0200

    frontend deployment issues fixed

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -28,7 +28,7 @@ function Register() {
 
     try {
       const response = await fetch(
-        `${import.meta.env.VITE_API_URL}/api/auth/register`,
+        `${process.env.REACT_APP_API_URL}/api/auth/register`,
         {
           method: "POST",
           headers: {

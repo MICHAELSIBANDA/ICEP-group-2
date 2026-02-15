91fb8aaa1363699758649d0cd953541bcafcd150
Lesego Mfikoe <mfikoelucky@gmail.com>
2026-02-15 19:35:28 +0200

    Register and Login links fix and Token display removed

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -152,9 +152,9 @@ function Register() {
 
       <p className="text-center text-sm text-gray-700 mt-6">
         Already have an account?{" "}
-        <a href="/login" className="text-blue-700 hover:underline">
+        <Link to="/login" className="text-blue-600 hover:underline">
           Login here
-        </a>
+        </Link>
       </p>
 
     </div>
